9463d48a4272540d31231270b2a0c2508a0a8d0f
Omar Hosny Keshk <58389695+omarhosny206@users.noreply.github.com>
2023-09-25 15:47:19 +0300

    Add logger

--- src/main/kotlin/com/example/BillReminderCronJob.kt
+++ src/main/kotlin/com/example/BillReminderCronJob.kt
@@ -1,9 +1,15 @@
+package com.example
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 
 @Component
 class BillReminderCronJob : CommandLineRunner {
+    private val log: Logger = LoggerFactory.getLogger(BillReminderCronJob::class.java)
+
     override fun run(vararg args: String?) {
-        println("Bill Reminder Cronjob")
+        log.info("Cronjob is Running")
     }
 }
